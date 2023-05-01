9076a6c4b4389da192e1499bf10408b0df0571fe
SergeiT <uraniumcore238@gmail.com>
2023-05-01 21:14:27 +0300

    no --headless

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -17,7 +17,7 @@ def load_env():
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--browser_ver", action="store", default="109.0")
-    parser.addoption("--headless", action="store", default=False)
+
     parser.addoption("--remote", action="store", default=False)
     parser.addoption("--hub", action="store", default="localhost")
 
@@ -27,26 +27,22 @@ def config(request):
     browser = request.config.getoption("--browser")
     version = request.config.getoption("--browser_ver")
     hub = request.config.getoption("--hub")
-    headless = bool(request.config.getoption("--headless"))
+
     remote = bool(request.config.getoption("--remote"))
     return {"remote": remote,
             "version": version,
             "browser": browser,
-            "headless": headless,
+
             "hub": hub}
 
 
 def get_chrome_options(config):
     options = ChromeOptions()
-    options.headless = config["headless"]
-    if config['headless']:
-        options.add_argument("--window-size=1920,1080")
     return options
 
 
 def get_firefox_options(config):
     options = FirefoxOptions()
-    options.headless = config["headless"]
     return options
 
 
